37ae8b8c171e67f16faf55d671ee30556a9c2819
sone 7502 <nebojsa.it@gmail.com>
2021-01-30 02:18:29 +0100

    stylized welcome page

--- Modules/Core/Views/welcome.php
+++ Modules/Core/Views/welcome.php
@@ -5,46 +5,30 @@
     <div class="row px-3">
         <div class="col-md-6 col-12 mb-4 p-0 p-md-3">
             <div class="card">
-                <div class="card-header">
-                    Model
-                </div>
-                <div class="card-body">
-                    <h5 class="card-title">Interacts with the database</h5>
-                    <p class="card-text">It receives, stores and retrieves data for the user.</p>
-                </div>
+                <div class="card-header">Model</div>
+                <div class="card-body"><h5 class="card-title">Interacts with the database</h5></div>
+                <div class="card-footer">It receives, stores and retrieves data for the user.</div>
             </div>
         </div>
         <div class="col-md-6 col-12 mb-4 p-0 p-md-3">
             <div class="card">
-                <div class="card-header">
-                    View
-                </div>
-                <div class="card-body">
-                    <h5 class="card-title">Return HTML page</h5>
-                    <p class="card-text">Displays information to the user and integrates data from the controller.</p>
-                </div>
+                <div class="card-header">View</div>
+                <div class="card-body"><h5 class="card-title">Return HTML page</h5></div>
+                <div class="card-footer">Displays information to the user and integrates data from the controller.</div>
             </div>
         </div>
         <div class="col-md-6 col-12 mb-4 p-0 p-md-3">
             <div class="card">
-                <div class="card-header">
-                    Controller
-                </div>
-                <div class="card-body">
-                    <h5 class="card-title">Connect Model & View</h5>
-                    <p class="card-text">Sends and receives data from the model and passes to the view.</p>
-                </div>
+                <div class="card-header">Controller</div>
+                <div class="card-body"><h5 class="card-title">Connect Model & View</h5></div>
+                <div class="card-footer">Sends and receives data from the model and passes to the view.</div>
             </div>
         </div>
         <div class="col-md-6 col-12 mb-4 p-0 p-md-3">
             <div class="card">
-                <div class="card-header">
-                    Route
-                </div>
-                <div class="card-body">
-                    <h5 class="card-title">Maps an HTTP request to a request handler</h5>
-                    <p class="card-text">Route transforms a path and converts into a call to a method..</p>
-                </div>
+                <div class="card-header">Route</div>
+                <div class="card-body"><h5 class="card-title">Maps an HTTP request to a request handler</h5></div>
+                <div class="card-footer">Route transforms a path and converts into a call to a method.</div>
             </div>
         </div>
     </div>
